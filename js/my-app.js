5cd392d63ab5bad718fbc5e79ac590a842c10324
bglky <yuandaozhou@qq.com>
2016-07-26 15:18:27 +0800

    optimize disqus load, it is perfect now

--- js/my-app.js
+++ js/my-app.js
@@ -1,80 +1,119 @@
+// load js function
+function loadJS(u, c, a = true, i = "") {
+	var d = document,
+		t = 'script',
+		o = d.createElement(t),
+		s = d.getElementsByTagName(t)[0];
+	o.async = true;
+	if (i) o.id = i;
+	o.src = u;
+	if (c) {
+		o.addEventListener('load', function(e) {
+			c(null, e);
+		}, false);
+	}
+	s.parentNode.insertBefore(o, s);
+}
+// load Disqus , it is async now,support spa
+function loadDisqus(commentDom) {
+	var disqus_username=$$('meta[name="disqus_username"]').attr("content");
+	commentDom.html('<div id="disqus_thread" class="disqus-thread"></div>');				
+	if ($$("script#disqus").length == 0) {
+		loadJS("http://"+disqus_username+".disqus.com/embed.js", function() {
+			var disqus_identifier = commentDom.data("disqus_identifier");
+			var disqus_url = commentDom.data("disqus_url");
+			var disqus_title = commentDom.data("disqus_title");
+			DISQUS.reset({
+				reload: true,
+				config: function() {
+					this.page.identifier = disqus_identifier;
+					this.page.url = disqus_url;
+					this.page.title = disqus_title;
+				}
+			});
+		}, true, "disqus");
+	} else {
+		var disqus_identifier = commentDom.data("disqus_identifier");
+		var disqus_url = commentDom.data("disqus_url");
+		var disqus_title = commentDom.data("disqus_title");
+		DISQUS.reset({
+			reload: true,
+			config: function() {
+				this.page.identifier = disqus_identifier;
+				this.page.url = disqus_url;
+				this.page.title = disqus_title;
+			}
+		});
+	}
+}
+// check whether load Disqus or not, if load,check which commentDom to load to,it is perfect now
+function checkDisqus() {
+	var commentDom = $$(".page").find(".comment");
+	if (commentDom.length > 0) {
+		commentDom.find("#disqus_thread").remove();
+		if (commentDom.length == 1) {
+			if ($$(".view-main").find(".page").length == 1 || $$(".page-from-left-to-center").find(".comment").length > 0 || $$(".page-on-center").find(".comment").length > 0) {
+				loadDisqus(commentDom);
+			}
+		} else {
+			if ($$(".page-from-left-to-center").find(".comment").length > 0) {
+				commentDom = $$(".page-from-left-to-center").find(".comment");
+			}
+			if ($$(".page-on-center").find(".comment").length > 0) {
+				commentDom = $$(".page-on-center").find(".comment");
+			}
+			loadDisqus(commentDom);
+		}
+	}
+}	
+
+
 // Initialize app
 var myApp = new Framework7({
 	material: true,
 	pushState: true,
 	pushStateSeparator: "#!",
 	cacheIgnore: ["/"],
-	showBarsOnPageScrollEnd: false,
-	init: false
+	showBarsOnPageScrollEnd: false
 });
-
-
-myApp.init();
 // Export selectors engine
 var $$ = Dom7;
-
 // Add view
-var mainView = myApp.addView('.view-main', {
-	//dynamicNavbar: true
+var mainView = myApp.addView('.view-main', {	
 });
-$$(document).on('pageAfterAnimation', '.page', function() {//一开始就进入有评论的页面时还有问题
-	$$("article a[href^='http']").addClass("external"); //博文里的外部链接添加class="external"
-	if($$(".page-on-left").find("#disqus_thread"))
-	{
-		$$(".page-on-left").find("#disqus_thread").remove();
-	}	
-	var commentDom=$$(".page-on-center").find(".comment");	
-	
-	if(commentDom.length>0)
-	{	
-		if(!commentDom.find("#disqus_thread"))
-		{
-			commentDom.html('<div id="disqus_thread" class="disqus-thread"></div>');
-		}		
-		var disqus_identifier=commentDom.data("disqus_identifier");
-		var disqus_url=commentDom.data("disqus_url");
-		var disqus_title=commentDom.data("disqus_title");
-		DISQUS.reset({
-			reload: true,
-			config: function() {
-				this.page.identifier = disqus_identifier;
-				this.page.url = disqus_url;
-				this.page.title= disqus_title;
-			}
-		});
-	}
+
+$$("article a[href^='http']").addClass("external");
+checkDisqus();
+
+$$(document).on('pageAfterAnimation', '.page', function() {
+	$$("article a[href^='http']").addClass("external"); //博文里的外部链接添加class="external"	
+	checkDisqus();
 })
-$$("article a[href^='http']").addClass("external");//博文里的外部链接添加class="external"
-//$$(document).once('pageAfterAnimation', '.page', function(e) {
+
+
+			
+//loadJS("http://hm.baidu.com/hm.js?3a12295eec4bd436a92e0b39d50f5b31",function(){console.log("loaded baidu")},true,"baidu_analytics");	
+			
+//百度统计
+//			
+//			var _hmt = _hmt || [];
+//			(function() {
+//			  var hm = document.createElement("script");
+//			  hm.id = "baidu_analytics";
+//			  hm.src = "//hm.baidu.com/hm.js?3a12295eec4bd436a92e0b39d50f5b31";
+//			  var s = document.getElementsByTagName("script")[0]; 
+//			  s.parentNode.insertBefore(hm, s);
+//			})();
+//
+//
+//$$(document).on('pageAfterAnimation', '.page', function(){
+//	var _hmt = _hmt || [];
 //	(function() {
-//		var dsq = document.createElement('script');
-//		dsq.type = 'text/javascript';
-//		dsq.async = true;
-//		dsq.src = '//bglky.disqus.com/embed.js';
-//		(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+//		$$("#baidu_analytics").remove();
+//		var hm = document.createElement("script");
+//		hm.id = "baidu_analytics";
+//		hm.src = "//hm.baidu.com/hm.js?3a12295eec4bd436a92e0b39d50f5b31";
+//		var s = document.getElementsByTagName("script")[0]; 
+//		s.parentNode.insertBefore(hm, s);
 //	})();
 //})
-			
-			
-			
-			var _hmt = _hmt || [];
-			(function() {
-			  var hm = document.createElement("script");
-			  hm.id = "baidu_analytics";
-			  hm.src = "//hm.baidu.com/hm.js?3a12295eec4bd436a92e0b39d50f5b31";
-			  var s = document.getElementsByTagName("script")[0]; 
-			  s.parentNode.insertBefore(hm, s);
-			})();
-
-
-$$(document).on('pageAfterAnimation', '.page', function(){
-	var _hmt = _hmt || [];
-	(function() {
-		$$("#baidu_analytics").remove();
-		var hm = document.createElement("script");
-		hm.id = "baidu_analytics";
-		hm.src = "//hm.baidu.com/hm.js?3a12295eec4bd436a92e0b39d50f5b31";
-		var s = document.getElementsByTagName("script")[0]; 
-		s.parentNode.insertBefore(hm, s);
-	})();
-})
